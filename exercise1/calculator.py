6713534756477186e096c7067201d0e05549e850
abdessamad1832 <abdessamadmounir720@gmail.com>
2025-03-11 22:14:45 +0000

    exercice 1 (+,-,*,/)
    exercice 1 (+,-,*,/)

--- exercise1/calculator.py
+++ exercise1/calculator.py
@@ -4,17 +4,9 @@ from typing import Union
 # Defining a Number type for cleaner type hints
 Number = Union[int, float]
 
-
 def add(a: Number, b: Number) -> Number:
     """
     Return the sum of a and b.
-
-    Args:
-        a: First number
-        b: Second number
-
-    Returns:
-        The sum of a and b
     """
     # TODO: Implement this function
     pass
@@ -23,28 +15,12 @@ def add(a: Number, b: Number) -> Number:
 def subtract(a: Number, b: Number) -> Number:
     """
     Return the result of subtracting b from a.
-
-    Args:
-        a: First number
-        b: Second number
-
-    Returns:
-        The result of a - b
     """
-    # TODO: Implement this function
-    pass
-
+    return a - b
 
 def multiply(a: Number, b: Number) -> Number:
     """
     Return the product of a and b.
-
-    Args:
-        a: First number
-        b: Second number
-
-    Returns:
-        The product of a and b
     """
     # TODO: Implement this function
     pass
@@ -53,16 +29,7 @@ def multiply(a: Number, b: Number) -> Number:
 def divide(a: Number, b: Number) -> Number:
     """
     Return the result of dividing a by b.
-
-    Args:
-        a: First number (dividend)
-        b: Second number (divisor)
-
-    Returns:
-        The result of a / b
-
-    Raises:
-        ValueError: If b is 0
+    Raises ValueError if b is 0.
     """
     # TODO: Implement this function
     pass
